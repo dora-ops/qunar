4ac9adcdd92187c9cc3e9ebf4c319ac902d6844c
liu Qihui <liuqihui@liudeMacBook-Pro.local>
2018-06-22 00:40:09 +0800

    fastclick、lib-flexible、reset.css、iconfont.css

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+
+import 'lib-flexible'
+import './assets/styles/reset.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+
+FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
