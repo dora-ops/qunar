578f19d58853482403793342033943dd67f4910a
liu Qihui <liuqihui@liudeMacBook-Pro.local>
2018-06-22 02:27:07 +0800

    引入vue-awesome-swiper

--- src/main.js
+++ src/main.js
@@ -4,19 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import FastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 import 'lib-flexible'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 
 FastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+	el: '#app',
+	router,
+	components: { App },
+	template: '<App/>'
 })
