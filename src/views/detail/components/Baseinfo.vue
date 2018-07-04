ee4b56d24efc286108ade2813b4c762f6b491083
liu Qihui <liuqihui@liudeMacBook-Pro.local>
2018-07-04 19:18:23 +0800

    新增详情组件Baseinfo.vue

---
+++ src/views/detail/components/Baseinfo.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="baseinfo">
+		baseinfo
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Baseinfo'
+}
+</script>
+
+<style lang="stylus" scoped>
+	.baseinfo
+		height 2048px
+</style>
